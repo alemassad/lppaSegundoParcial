8039f92c0fa7a2032bd38ad71ea1f8b57f37d179
Luis massad <massadalejandro@gmail.com>
2022-07-06 22:34:38 -0300

    version beta 2

--- dashboard.js
+++ dashboard.js
@@ -1,35 +1,47 @@
-const rellena = document.querySelector(".contenedor");
-const peticion = async()=>{
-    try{
-        const respuesta= await fetch("https://basic-server-one.vercel.app/users"); 
-          
-            
-            const datos= await respuesta.json();
-            console.log(datos);   
-            let elementos="";  
-            let subElementos="";
-          
-             datos.data.forEach(elem=>{
-                console.log(elem);
-                elementos+=`<tr class='table-tr'>
-                    <td class='table-td'>${elem.id}</td>
-                    <td class='table-td2'>${elem.name}</td>
-                    <td class='table-td3'>${elem.username}</td>
-                    <td class='table-td4'>${elem.email}</td>
-                    <td class='table-td5'>${elem.address.street}</td>
-                    <td class='table-td6'>${elem.address.suite}</td>
-                    <td class='table-td7'>${elem.address.city}</td>
-                    <td class='table-td8'>${elem.phone}</td>
-                    <td class='table-td9'>${elem.company.name}</td>
-                    </tr>`;
-                                                    
-            }); 
-            document.getElementById("table-tr").innerHTML= elementos;
-            
-        } catch(error){
-            alert(error);
+window.onload=function(){
+    console.log(localStorage.getItem("error"));
+    document.getElementById("btnLimpiar").onclick=limpiar;
+    if (!localStorage.getItem('error')) {
+       console.log("Resultados...");
+      
+    } else {  
+       
+        const peticion = async()=>{
+            try{
+                const respuesta= await fetch("https://basic-server-one.vercel.app/users");            
+                    
+                    const datos= await respuesta.json();
+                    console.log(datos);   
+                    let elementos="";  
+                    let subElementos="";
+                
+                    datos.data.forEach(elem=>{
+                        console.log(elem);
+                        elementos+=`<tr class='table'>
+                            <td class='td'>${elem.id}</td>
+                            <td class='td'>${elem.name}</td>
+                            <td class='td'>${elem.username}</td>
+                            <td class='td'>${elem.email}</td>
+                            <td class='td'>${elem.address.street}</td>
+                            <td class='td'>${elem.address.suite}</td>
+                            <td class='td'>${elem.address.city}</td>
+                            <td class='td'>${elem.phone}</td>
+                            <td class='td'>${elem.company.name}</td>
+                            </tr>`;
+                                                            
+                    }); 
+                    document.getElementById("table").innerHTML= elementos;
+                  
+                } catch(error){
+                    alert(error);
+                }
         }
+        peticion();        
+    }
+    function limpiar(){
+        localStorage.removeItem("error");
+        localStorage.removeItem("mensaje");
+        console.log("dentro del submit");
+        location.href="index.html";
+    }
 }
-peticion();
-
-
