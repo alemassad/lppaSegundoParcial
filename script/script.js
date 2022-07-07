40d033e3d68b48f0dd48147507547f60a8baaae8
Luis massad <massadalejandro@gmail.com>
2022-07-06 23:19:56 -0300

    version final

--- script/script.js
+++ script/script.js
@@ -1,37 +1,36 @@
  window.onload=function(){
-    console.log(localStorage.getItem('error'))
-    if (localStorage.getItem('error')=="false") {
+    
+    if (localStorage.getItem("error")=="false") {
        console.log("Bienvenido nuevamente");
        location.href="dashboard.html";
     } else {  
 
-    var form=document.getElementById("formulario")
-    formulario.reset();
-        form.addEventListener('submit', e =>{
+        var form=document.getElementById("formulario")
+        document.getElementById("input-correo").focus();
+        formulario.reset();
+            form.addEventListener("submit", e =>{
 
-            const inputNombre =document.getElementById("input-nombre").value;
+            const inputPass =document.getElementById("input-nombre").value;
             const inputMail=document.getElementById("input-correo").value;
                         
             e.preventDefault();
 
-            function verificarNombre(){
+            function verificarPass(){
                 
-                if(inputNombre.length<3||inputNombre.length==""){
+                if(inputPass.length<3||inputPass.length==""){
                     document.getElementById("error-nombre").style.display ="block";
-                    rojo("nombre");              
-                    console.log("ingrese un Nombre verdadero "+!isNaN(inputNombre));
+                    rojo("nombre");  
                     document.getElementById("error-nombre").style.display ="block";
                     rojo("nombre");
                     return false;        
-                }else if(!isNaN(inputNombre)){
+                }else if(!isNaN(inputPass)){
                     document.getElementById("error-nombre").style.display ="block";
                     rojo("nombre");   
                     return false;     
                 }else{
                     document.getElementById("valido-nombre").style.display="block";
                     verde("nombre");
-                    console.log("Nombre es ==> "+inputNombre);     
-                    nombreValido=inputNombre;   
+                    nombreValido=inputPass.toString();   
                     return true;
                 } 
             }  
@@ -40,19 +39,19 @@
                 
                 var regexEmail= /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/
                 if(!regexEmail.test(inputMail)){
-                    console.log("entrar un mail valido");
+                    alert("entrar un mail valido");
                     document.getElementById("error-correo").style.display ="block";
                     rojo("correo");
                 }else{
                     document.getElementById("valido-correo").style.display ="block";
                     verde("correo");
                     console.log("Email es ==> "+inputMail);
-                    correoValido=" "+inputMail;
+                    correoValido=inputMail.toString();
                     return true;
                 }
             }
 
-            if ( verificarNombre() && verificarCorreo()){
+            if ( verificarPass() && verificarCorreo()){
                 const peticion = async()=>{
                     try{
                             const respuesta= await fetch("https://basic-server-one.vercel.app/login", {
@@ -61,26 +60,24 @@
                                 "Content-Type": "application/json",
                             },
                             body: JSON.stringify({
-                                email: "valeria@gmail.com",
-                                password: "lppa2022"
+                                email:correoValido, //"valeria@gmail.com", 
+                                password:nombreValido //"lppa2022"    
                                 })
                             })
                         const datos= await respuesta.json();
-                        console.log(datos);                   
+                                          
                         if(datos.error==true){
                             alert("Los datos ingresados no son válidos")
                         }else {
                             var reqError= datos.error;
                             var reqMessage= datos.message
-                            localStorage.setItem('error',reqError);
-                            localStorage.setItem('mensaje',reqMessage);
-                            
-                            console.log("Error: "+reqError+"- Mensaje: "+reqMessage)
+                            localStorage.setItem("error",reqError);
+                            localStorage.setItem("mensaje",reqMessage);
                             location.href="dashboard.html";
                         }
 
                     }catch(error){
-                        console.log("HORROR ==>"+error);
+                        
                         alert(error);               
                     }
                 }
